d9ebd94b9ceedf77faaa4f59fc136e252267e742
Lennart Regebro <lregebro@redhat.com>
2016-01-07 21:55:32 +0100

    Command for analyzing logkeys logs

---
+++ keyboardtools/analyze.py
@@ -0,0 +1,57 @@
+import argparse
+import collections
+import json
+import re
+
+
+MODIFIERS = ('<lshft>', '<rshft>', '<lctrl>', '<rctrl>', '<lmeta>',
+             '<rmeta>', '<lalt>', '<ralt>', '<altgr>')
+
+class KeyStats(object):
+
+    def __init__(self):
+        self.keys = {}
+
+    def keypress(self, key, modifiers):
+        if key not in self.keys:
+            self.keys[key] = {'key': key, 'count': 0}
+            for modifier in MODIFIERS:
+                self.keys[key][modifier] = 0
+
+        self.keys[key]['count'] += 1
+        for modifier in modifiers:
+            self.keys[key][modifier] += 1
+
+
+def logkeys():
+    parser = argparse.ArgumentParser(
+        description='Analyze a logkeys log to generate statistics.')
+
+    parser.add_argument('infile', help='Path to the logkeys log file.')
+    parser.add_argument('outfile', help='Path to create the analysis file.')
+
+    args = parser.parse_args()
+    with open(args.infile, 'rt') as infile, open(args.outfile, 'wt') as outfile:
+        stats = _logkeys_analyze(infile)
+        json.dump(stats.keys, outfile)
+
+
+def _logkeys_analyze(infile):
+    stats = KeyStats()
+
+    for line in infile:
+        if not ' > ' in line:
+            continue
+
+        timestamp, logged = line.lower().split(' > ', 1)
+        keys = logged.replace('\n', '<enter>')
+        keys = re.findall('\<\w*?\>|.', keys)
+        modifiers = []
+        for key in keys:
+            if key in MODIFIERS:
+                modifiers.append(key)
+                continue
+            stats.keypress(key, modifiers)
+            modifiers = []
+
+    return stats
---
+++ tests/test_analyze.py
@@ -0,0 +1,82 @@
+import contextlib
+import io
+import json
+import os
+import sys
+import tempfile
+import unittest
+
+from keyboardtools.analyze import logkeys, _logkeys_analyze
+from pkg_resources import resource_string
+
+
+@contextlib.contextmanager
+def suppress_output():
+    stdout = io.StringIO()
+    old_stdout = sys.stdout
+    old_stderr = sys.stderr
+    sys.stdout = stdout
+    sys.stderr = stdout
+    yield stdout
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+
+
+class TestLogkeysParse(unittest.TestCase):
+
+    def test_incorrect_commandline(self):
+        with suppress_output():
+            sys.argv = ['kbt-logkeys']
+            self.assertRaises(SystemExit, logkeys)
+
+            sys.argv = ['kbt-logkeys', 'foo']
+            self.assertRaises(SystemExit, logkeys)
+
+    def test_file_output(self):
+        with suppress_output(), tempfile.TemporaryDirectory() as tempdir:
+            with open(os.path.join(tempdir, 'infile'), 'wb') as infile, \
+                 open(os.path.join(tempdir, 'outfile'), 'wt') as outfile:
+                    test_data = resource_string(__name__, 'logkeys.log')
+                    infile.write(test_data)
+                    infile.flush()
+                    sys.argv = ['kbt-logkeys', infile.name, outfile.name]
+                    logkeys()
+
+                    result = json.load(open(outfile.name, 'rt'))
+
+        self.assertEqual(
+            result['k'],
+            {'key': 'k', '<rshft>': 0, 'count': 9, '<lmeta>': 0,
+             '<lshft>': 0, '<rctrl>': 0, '<lctrl>': 0, '<ralt>': 0,
+             '<rmeta>': 0, '<altgr>': 0, '<lalt>': 0})
+        self.assertEqual(
+          result['ö'],
+          {'key': 'ö', '<rshft>': 0, 'count': 3, '<lmeta>': 0,
+           '<lshft>': 0, '<rctrl>': 0, '<lctrl>': 0, '<ralt>': 0,
+           '<rmeta>': 0, '<altgr>': 0, '<lalt>': 0})
+        self.assertEqual(
+          result['f'],
+          {'key': 'f', '<rshft>': 0, 'count': 7, '<lmeta>': 0,
+           '<lshft>': 1, '<rctrl>': 0, '<lctrl>': 1, '<ralt>': 0,
+           '<rmeta>': 0, '<altgr>': 0, '<lalt>': 0})
+
+    def test_analyze(self):
+        test_data = resource_string(__name__, 'logkeys.log').decode('UTF-8')
+        test_file = io.StringIO(test_data)
+        result = _logkeys_analyze(test_file)
+
+        self.assertEqual(
+            result.keys['k'],
+            {'key': 'k', '<rshft>': 0, 'count': 9, '<lmeta>': 0,
+             '<lshft>': 0, '<rctrl>': 0, '<lctrl>': 0, '<ralt>': 0,
+             '<rmeta>': 0, '<altgr>': 0, '<lalt>': 0})
+        self.assertEqual(
+            result.keys['ö'],
+            {'key': 'ö', '<rshft>': 0, 'count': 3, '<lmeta>': 0,
+             '<lshft>': 0, '<rctrl>': 0, '<lctrl>': 0, '<ralt>': 0,
+             '<rmeta>': 0, '<altgr>': 0, '<lalt>': 0})
+        self.assertEqual(
+          result.keys['f'],
+          {'key': 'f', '<rshft>': 0, 'count': 7, '<lmeta>': 0,
+           '<lshft>': 1, '<rctrl>': 0, '<lctrl>': 1, '<ralt>': 0,
+           '<rmeta>': 0, '<altgr>': 0, '<lalt>': 0})
